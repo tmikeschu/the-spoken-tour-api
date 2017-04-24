fb5fee2d96a20808238fa27b581b032de43b2c58
Mike Schutte <tmikeschutte@gmail.com>
2017-04-24 16:59:50 -0600

    Add mailer and background worker
    When a user makes a suggestion, an email job is queued
    * Add procfile
    * Add gems for background work

--- spec/mailers/previews/suggestion_mailer_preview.rb
+++ spec/mailers/previews/suggestion_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/suggestion_mailer
 class SuggestionMailerPreview < ActionMailer::Preview
-
+  def suggestion_preview
+    pin = FactoryGirl.create(:suggestion_pin)
+    SuggestionMailer.suggestion(pin)
+  end
 end
