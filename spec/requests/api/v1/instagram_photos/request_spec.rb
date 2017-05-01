b210ff89ac71a760bfad8ececeeaf02b4cf776c2
Mike Schutte <tmikeschutte@gmail.com>
2017-04-30 21:28:00 -0600

    Improve description blocks on request specs
    * Include verb and path

--- spec/requests/api/v1/instagram_photos/request_spec.rb
+++ spec/requests/api/v1/instagram_photos/request_spec.rb
@@ -5,16 +5,18 @@ RSpec.describe "Instagram API filter", type: :request do
     allow_any_instance_of(ApplicationController).to receive(:authorized?).and_return(true)
   end
 
-  it "returns a list of all photos from The Spoken Tour account" do
-    VCR.use_cassette("instagram") do
-      get api_v1_instagram_photos_path
+  describe "GET /api/v1/instagram_photos" do
+    it "returns a list of all photos from The Spoken Tour account" do
+      VCR.use_cassette("instagram") do
+        get api_v1_instagram_photos_path
 
-      expect(response).to be_success
+        expect(response).to be_success
 
-      photos = JSON.parse(response.body, symbolize_names: true)
-      photo  = photos.first
+        photos = JSON.parse(response.body, symbolize_names: true)
+        photo  = photos.first
 
-      expect(photo.keys).to match_array([:caption, :image, :link])
+        expect(photo.keys).to match_array([:caption, :image, :link])
+      end
     end
   end
 end
