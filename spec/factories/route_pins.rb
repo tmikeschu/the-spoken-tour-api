0d867605defef71a50a06da8dd41984231e8c2a0
Mike Schutte <tmikeschutte@gmail.com>
2017-05-21 21:59:03 -0600

    Extract lat/lng setting to trait

--- spec/factories/route_pins.rb
+++ spec/factories/route_pins.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :route_pin do
-    lat       { Faker::Address.latitude }
-    lng       { Faker::Address.longitude }
+    lat_lng
   end
 end
---
+++ spec/factories/traits.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  trait :lat_lng do
+    lat { Faker::Address.latitude }
+    lng { Faker::Address.longitude }
+  end
+end
