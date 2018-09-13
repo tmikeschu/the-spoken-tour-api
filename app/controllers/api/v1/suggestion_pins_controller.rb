9674096ad4d97e99ec30cdac5c640f437bdb1669
Mike Schutte <tmikeschutte@gmail.com>
2018-09-13 10:57:24 -0400

    Lint/refactor suggestion pins controller

--- app/controllers/api/v1/suggestion_pins_controller.rb
+++ app/controllers/api/v1/suggestion_pins_controller.rb
@@ -1,3 +1,6 @@
+# SuggestionPinsController handles requests for reading all suggestions and for
+# creating one from the react client.
+
 class Api::V1::SuggestionPinsController < ApplicationController
   def index
     render json: SuggestionPin.all
@@ -6,7 +9,7 @@ class Api::V1::SuggestionPinsController < ApplicationController
   def create
     pin = SuggestionPin.new(pin_params)
     if pin.save!
-			SendEmailJob.set(wait: 20.seconds).perform_later(pin)
+      SendEmailJob.set(wait: 20.seconds).perform_later(pin)
       render json: pin, status: 201
     else
       render json: pin.errors.full_messages.join(", ")
@@ -21,10 +24,16 @@ class Api::V1::SuggestionPinsController < ApplicationController
   private
 
   def pin_params
-    strong = params.require(:pin).permit(
-      :label, :lat, :lng, :description, :message, :category, :email, :name
-    ).to_h
-    strong["category"] = strong["category"].to_i if strong["category"]
-    strong
+    params.require(:pin).permit(
+      :label, :lat, :lng, :description, :message, :email, :name
+    ).merge(category)
+  end
+
+  def category
+    if params.key?(:category)
+      { category: params.fetch(category).to_i }
+    else
+      {}
+    end
   end
 end
