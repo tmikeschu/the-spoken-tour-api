187fdcf65a3700b92dca5b0e134b0bf98cbc8ea8
Mike Schutte <tmikeschutte@gmail.com>
2021-12-11 13:50:39 -0500

    fix faradar connection

--- app/services/inreach_service.rb
+++ app/services/inreach_service.rb
@@ -4,12 +4,12 @@ class InreachService
   end
 
   def latest_coordinates
-    { 
+    {
       location: {
         lat: latitude(response_data), lng: longitude(response_data)
       },
       date: date(response_data)
-    }  
+    }
   end
 
   def latitude(response_data)
@@ -24,11 +24,12 @@ class InreachService
     Nokogiri.XML(response_data).css("Data[name='Time'] value").text
   end
 
-  private 
+  private
 
   def conn
-    @conn ||= Faraday.new("https://inreach.garmin.com")
-    @conn.basic_auth(ENV["inreach_user"], ENV["inreach_pw"])
+    @conn ||= Faraday.new("https://inreach.garmin.com") do |conn|
+      conn.request(:basic_auth, ENV["inreach_user"], ENV["inreach_pw"])
+    end
     @conn
   end
 
